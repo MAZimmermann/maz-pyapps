ed71107f15817ce094bb5b7ccc0a433838e58ae4
MAZimmermann <MAZimmermann26@gmail.com>
2018-07-19 15:49:12 -0400

    Built html template to display list of stock data

--- application.py
+++ application.py
@@ -3,7 +3,7 @@
 """
 
 # Import statement for flask
-from flask import Flask
+from flask import Flask, render_template
 
 # Import statement for function I wrote in ticker.py
 from ticker import grabTickerInfo
@@ -11,9 +11,9 @@ from ticker import grabTickerInfo
 # Print a welcome message
 def welcome(ticker = " "):
     if ticker == " ":
-        return '<p>Welcome! Append a valid ticker symbol to the url and see what happens...</p>\n'
+        return '<p>Welcome! Append a valid ticker symbol to the url for today\'s stats</p>\n'
     else:
-        return '<p>Company: %s </p>\n' % ticker
+        return '<p>Company: %s </p>\n' % ticker.upper()
 
 # Set title for webpage
 header_text = '''<html>\n<head> <title>MAZ</title> </head>\n<body>'''
@@ -27,7 +27,7 @@ application.add_url_rule('/', 'index', (lambda: header_text +
 
 # Add a rule when the page is accessed with a ticker appended to the site
 application.add_url_rule('/<ticker>', 'hello', (lambda ticker:
-    header_text + welcome(ticker) + grabTickerInfo(ticker)))
+    header_text + welcome(ticker) + render_template('iter.html', info=grabTickerInfo(ticker))))
 
 # run the app.
 if __name__ == "__main__":
--- ticker.py
+++ ticker.py
@@ -19,11 +19,16 @@ def grabTickerInfo(appended):
     resp = requests.get(url)
     
     # Make new beautiful soup object
-    soup = BeautifulSoup.BeautifulSoup(resp.text, "html.parser")
+    soup = BeautifulSoup(resp.text, "html.parser")
     
     # Set dataList to the bundle of elements/tags nested within class: 'list list--kv list--col50'
     dataList = soup.find('ul', {'class': 'list list--kv list--col50'})
     
+    stockInfo = []
+    
     # Iterate through dataList and print dataListItem.text
     for dataListItem in dataList.findAll('li', {'class': 'kv__item'}):
-        return dataListItem.text
+        stockInfo.append(dataListItem.text)
+
+    # Return list to be passed into our html template
+    return stockInfo
